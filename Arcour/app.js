1903aee7d5381641385314976db709d33bf1f972
Noé Esteves <121628116+Bunny1979@users.noreply.github.com>
2023-07-03 19:38:44 -0300

    Add files via upload
    Envío archivo app con los archivos agregados, archivo home confeccionado y archivo script

--- Arcour/app.js
+++ Arcour/app.js
@@ -1,27 +1,54 @@
-let express = require('express');
-let app = express();
-let path = require('path');
+const express = require("express");
+const path = require("path");
 
-let publicPath = path.join(dirname,'./public')
-app.use(express.static(publicPath))
+const app = express();
 
-app.get('/index' ,(req,res) => {
-res.sendFile(path.join(dirname, './views/index.html'))
-})
-app.get('/login' ,(req,res) => {
-    res.sendFile(path.join(dirname, './views/login.html'))
-})
-app.get('/productCart' ,(req,res) => {
-    res.sendFile(path.join(dirname, './views/productCart.html'))
-})
-app.get('/productDetail' ,(req,res) => {
-    res.sendFile(path.join(dirname, './views/productDetail.html'))
-})
-app.get('/register' ,(req,res) => {
-    res.sendFile(path.join(dirname, './views/register.html'))
-})
+app.use(express.static("public"));
 
-app.listen(3000,()=> {
-console.log('Servidor exitoso')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
 
+app.get("/contacto.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/contacto.html"));
+});
+
+app.get("/nuestra-empresa.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/nuestra-empresa.html"));
+});
+
+app.get("/empresas-amigas.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/empresas-amigas.html"));
+});
+
+app.get("/buenosAires.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/buenosAires.html"));
+});
+
+app.get("/bogota.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/bogota.html"));
+});
+
+app.get("/montevideo.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/montevideo.html"));
+});
+
+app.get("/login.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.get("/register.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/product-cart.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/product-cart.html"));
+});
+
+app.get("/product-detail.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/product-detail.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en puerto 3000 | http://localhost:3000");
+});
