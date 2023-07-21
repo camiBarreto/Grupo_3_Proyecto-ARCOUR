7a5fd548afcc602120e6c482aac93dea1745abdf
samuel20468 <samuelrivera639@gmail.com>
2023-07-21 00:54:13 -0500

    cambio de rutas y modificacion app.js

--- Arcour/app.js
+++ Arcour/app.js
@@ -1,53 +1,19 @@
 const express = require("express");
 const path = require("path");
+const mainRout = require("./routers/mainRouter");
+const usersRout = require("./routers/usersRouter");
+const productsRout = require("./routers/productRouter");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
-});
+app.use("/", mainRout);
 
-app.get("/contacto", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/contacto.html"));
-});
+app.use("/users", usersRout);
 
-app.get("/nuestra-empresa", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/nuestra-empresa.html"));
-});
+app.use("/products", productsRout);
 
-app.get("/empresas-amigas", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/empresas-amigas.html"));
-});
-
-app.get("/buenosAires", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/buenosAires.html"));
-});
-
-app.get("/bogota", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/bogota.html"));
-});
-
-app.get("/montevideo", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/montevideo.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/register.html"));
-});
-
-app.get("/product-cart", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/productCart.html"));
-});
-
-app.get("/product-detail", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/product-detail.html"));
-});
 
 app.listen(3000, () => {
   console.log("Servidor escuchando en puerto 3000 | http://localhost:3000");
