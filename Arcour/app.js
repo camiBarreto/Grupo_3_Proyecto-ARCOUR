7a6704b44b53f6e02db942ca72726b2afb5b92b0
marianoCosco <mariancosco1406@gmail.com>
2023-08-05 20:33:08 -0300

    creacion del producto

--- Arcour/app.js
+++ Arcour/app.js
@@ -42,4 +42,5 @@ app.listen(port || 3000, () => {
 /*  1. Controller de User que se conecte con los modelos respectivos, para manejar los datos de users.json.
     2. Crear la vista de formulario de creacion de productos ejs. 
     3. Conectar el product-detail con los datos de los productos para mostrarlos de forma dinámica.
+    4. Agregar barra de busqueda del home.
 */
