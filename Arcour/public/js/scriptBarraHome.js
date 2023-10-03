e4445dd48dba9fe2b3df625cc0fb87ed82795606
samuel20468 <136033542+samuel20468@users.noreply.github.com>
2023-10-03 00:00:15 -0500

    interactividad

--- Arcour/public/js/scriptBarraHome.js
+++ Arcour/public/js/scriptBarraHome.js
@@ -1,17 +1,42 @@
+//Script de solo ida / ida y vuelta
+
 const radioIda = document.getElementById('ida');
 const radioIdaVuelta = document.getElementById('idaVuelta');
 const seccionArribo = document.getElementById('seccionArribo'); // Agrega un ID a la sección de Arribo en tu HTML
 
-radioIda.addEventListener('click', () => {
-    if (radioIda.checked) {
-        radioIdaVuelta.checked = false;
-        seccionArribo.style.display = 'none';
+window.addEventListener("load", () => {
+    if(radioIda) {
+        radioIda.addEventListener('click', () => {
+            if (radioIda.checked) {
+                radioIdaVuelta.checked = false;
+                seccionArribo.style.display = 'none';
+            }
+        });
     }
-});
+    
+    if(radioIdaVuelta) {
+        radioIdaVuelta.addEventListener('click', () => {
+            if (radioIdaVuelta.checked) {
+                radioIda.checked = false;
+                seccionArribo.style.display = 'block';
+            }   
+        });
+    }
+    
+})
 
-radioIdaVuelta.addEventListener('click', () => {
-    if (radioIdaVuelta.checked) {
-        radioIda.checked = false;
-        seccionArribo.style.display = 'block';
+//Script SweetAlert product-detail
+
+window.addEventListener("load", () => {
+    const validation = document.getElementById("error");
+    if(validation) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            html: '<div><p class="notFound-text">No se encontraron vuelos</p> <a href="/" class="back-home-link">Volver a la página de inicio</a></div>',
+            showConfirmButton: false,
+            showCloseButton: true,
+            allowOutsideClick: false,
+          })
     }
-});
+})
--- Arcour/public/js/scriptHeader.js
+++ Arcour/public/js/scriptHeader.js
@@ -1,9 +1,11 @@
 const profileDropDown = document.getElementById("profile-dropdown");
 const dropdownMenu = document.getElementById("dropdown-menu");
 
-profileDropDown.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("show-menu");
-})
+window.addEventListener("load", () => {
+    profileDropDown.addEventListener("click", () => {
+        dropdownMenu.classList.toggle("show-menu");
+    });
+});
 
 
 
